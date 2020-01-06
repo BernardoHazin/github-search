3ea3f4d225214858004a68b3e02569751aefba7e
hazin <bernardo.hazin@mesainc.com.br>
2020-01-06 11:53:05 -0300

    Setting up token

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -3,8 +3,7 @@ import ApolloClient from 'apollo-boost'
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   headers: {
-    Authorization:
-      process.env.AUTH_TOKEN || 'token 5038a506026f4ad3d98cc1d483cb9470e33de5d4'
+    Authorization: process.env.AUTH_TOKEN
   }
 })
 
